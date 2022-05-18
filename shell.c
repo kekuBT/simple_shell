a79b0954247d27ef619ef4e8ef6eeeba5b57e0de
kekubt <kekubt@gmail.com>
2022-05-18 16:32:14 +0300

    1st question

--- shell.c
+++ shell.c
@@ -5,60 +5,24 @@
 int main(int argc, char **argv)
 {
 
-    int maxValue = 10;
-    srand(time(NULL));
-    int random = rand() % maxValue + 1;
-    printf("Welcome, this is a guessing game.\nYou have three chances so use wisely from 1 - 10.\n");
+	int maxValue = 10;
 
-    int val = printf("Your first guess is - ");
-    scanf("%d", &val);
+	srand(time(NULL));
+	int random = rand() % maxValue + 1;
 
-    if (val == random)
-    {
-        printf("Wow you got it on your first try.\n");
-    }
-    else
-    {
-        printf("Wrong, two attempts left. ");
-        if (val >= random)
-        {
-            printf("Too high, go lower.\n");
-        }
-        else if (val <= random)
-        {
-            printf("Too low, go higher.\n");
-        }
-        int val = printf("Your second guess is - ");
-        scanf("%d", &val);
+	printf("Welcome, this is a guessing game from 1 - 10.\n");
 
-        if (val == random)
-        {
-            printf("Well done you got it on your second try.\n");
-        }
-        else
-        {
-            printf("Wrong, one attempt left.\n");
-            if (val >= random)
-            {
-                printf("Too high, go lower.\n");
-            }
-            else if (val <= random)
-            {
-                printf("Too low, go higher.\n");
-            }
-            int val = printf("Your last guess is - ");
-            scanf("%d", &val);
+	int val = printf("Your first guess is - ");
 
-            if (val == random)
-            {
-                printf("You almost missed it but you did it, it is %d.\n", random);
-            }
-            else
-            {
-                printf("Wrong again, that was your last chance.\nThe answer was %d.\n", random);
-            }
-        }
-    }
+	scanf("%d", &val);
 
-    return (0);
-} 
+	if (val == random)
+	{
+	printf("Wow you got it on your first try.\n");
+	}
+	else
+	{
+	printf("Wrong, you failed. ");
+	}
+	return (0);
+}
